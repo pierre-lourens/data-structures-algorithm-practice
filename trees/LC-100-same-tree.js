96fad93a7fa8f21bd0127e876937e675b840123b
Pierre Lourens <plourens3@gmail.com>
2023-04-20 08:17:14 -0700

    add LC-100 same tree

---
+++ trees/LC-100-same-tree.js
@@ -0,0 +1,19 @@
+/*
+Title: 100. Same tree
+URL: https://leetcode.com/problems/same-tree/description/
+
+Note: A simple traversal works, just need to add base cases to reflect the checking.
+*/
+
+function isSameTree(p, q) {
+  if (!p && !q) {
+    return true;
+  }
+  if ((!p && q) || (!q && p)) {
+    return false;
+  }
+  if (p.val !== q.val) {
+    return false;
+  }
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
